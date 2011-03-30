be5486ec21318c6209998a1760506a9497dc5d82
Kristoffer Hammer <kristoffer@hammernet.org>
2011-03-30 13:37:09 +0200

    Index page for the video first version
    Signed-off-by: Kristoffer Hammer <kristoffer@hammernet.org>

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+
+</head>
+
+<body>
+<?php
+$dir = "videos";
+
+// Open a known directory, and proceed to read its contents
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            
+            echo "<video src='videos/$file' controls='controls' controls='fullscreen'>";
+			echo "your browser does not support the video tag";
+			echo "</video>";
+
+            
+            echo "filename: $file : filetype: " . filetype($dir . $file) . "\n" . "</br>";
+            
+        }
+        closedir($dh);
+    }
+}
+?>
+</body>
+</html>
